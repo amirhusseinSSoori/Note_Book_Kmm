d541c58281aab7c5fc0b2aed0be684d3b995f00d
amirhusseinSSoori <amirhusseinsppro75@gmail.com>
2023-01-14 01:15:12 +0330

    Add NoteApp for di and used

---
+++ androidApp/src/main/java/com/amirhusseinSoori/notebookkmm/android/NoteApp.kt
@@ -0,0 +1,7 @@
+package com.amirhusseinSoori.notebookkmm.android
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NoteApp: Application()
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("com.android.library").version("7.4.0").apply(false)
     kotlin("android").version("1.7.10").apply(false)
     kotlin("multiplatform").version("1.7.10").apply(false)
-    id ("com.google.dagger.hilt.android").version ("2.44") apply (false)
+    id ("com.google.dagger.hilt.android").version ("2.42") apply (false)
     id("com.squareup.sqldelight").version ("1.5.4") apply (false)
 }
 
