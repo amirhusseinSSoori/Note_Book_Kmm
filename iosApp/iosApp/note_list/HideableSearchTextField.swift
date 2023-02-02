2e10a29466d27c022091ad77079c1a6e844f030a
amirhusseinSSoori <amirhusseinsppro75@gmail.com>
2023-02-02 23:51:24 +0330

    change preview

--- iosApp/iosApp/note_list/HideableSearchTextField.swift
+++ iosApp/iosApp/note_list/HideableSearchTextField.swift
@@ -1,44 +1,39 @@
 import SwiftUI
 
-struct HideableSearchTextField<Destination:View>: View {
+struct HideableSearchTextField<Destination: View>: View {
     
     var onSearchToggled: () -> Void
-    var destinnationProvider: () -> Destination
-    var isSearchActive:Bool
-    @Binding var searchText:String
+    var destinationProvider: () -> Destination
+    var isSearchActive: Bool
+    @Binding var searchText: String
     
     var body: some View {
-        HStack{
-            TextField("Search....",text:$searchText)
+        HStack {
+            TextField("Search...", text: $searchText)
                 .textFieldStyle(.roundedBorder)
                 .opacity(isSearchActive ? 1 : 0)
-            
-            
-            if(!isSearchActive){
+            if !isSearchActive {
                 Spacer()
             }
-            
-            Button(action:onSearchToggled){
+            Button(action: onSearchToggled) {
                 Image(systemName: isSearchActive ? "xmark" : "magnifyingglass")
+                    .foregroundColor(.black)
             }
-            
-            NavigationLink(destination: destinnationProvider()){
+            NavigationLink(destination: destinationProvider()) {
                 Image(systemName: "plus")
+                    .foregroundColor(.black)
             }
-    
-
         }
     }
 }
 
 struct HideableSearchTextField_Previews: PreviewProvider {
     static var previews: some View {
-        HideableSearchTextField(onSearchToggled: {
-       
-            
-        }, destinnationProvider: {
-            EmptyView()
-            
-        }, isSearchActive: true, searchText: .constant("search"))
+        HideableSearchTextField(
+            onSearchToggled: {},
+            destinationProvider: { EmptyView() },
+            isSearchActive: true,
+            searchText: .constant("YouTube")
+        )
     }
 }
