7ce8c46f46a7c97a8b819b24ff3e99488a9fa26a
amirss <amirhusseinsoori75@gmail.com>
2023-04-11 14:40:54 +0330

    update dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.3.0"
+        kotlinCompilerExtensionVersion = "1.4.1"
     }
     packagingOptions {
         resources {
@@ -38,21 +38,21 @@ android {
 
 dependencies {
     implementation(project(":shared"))
-    implementation("androidx.compose.ui:ui:1.3.3")
-    implementation("androidx.compose.ui:ui-tooling:1.3.3")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.3.3")
-    implementation("androidx.compose.foundation:foundation:1.3.1")
-    implementation("androidx.compose.material:material:1.3.1")
-    implementation("androidx.activity:activity-compose:1.6.1")
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.0")
+    implementation("androidx.compose.ui:ui:1.4.1")
+    implementation("androidx.compose.ui:ui-tooling:1.4.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.4.1")
+    implementation("androidx.compose.foundation:foundation:1.4.1")
+    implementation("androidx.compose.material:material:1.4.1")
+    implementation("androidx.activity:activity-compose:1.7.0")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
 
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
 
-    implementation("com.google.dagger:hilt-android:2.42")
-    kapt("com.google.dagger:hilt-android-compiler:2.42")
-//    kapt("androidx.hilt:hilt-compiler:2.44")
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
 
 
+    implementation ("androidx.navigation:navigation-compose:2.5.3")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 }
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    id("com.android.application").version("7.4.0").apply(false)
-    id("com.android.library").version("7.4.0").apply(false)
-    kotlin("android").version("1.7.10").apply(false)
-    kotlin("multiplatform").version("1.7.10").apply(false)
-    id ("com.google.dagger.hilt.android").version ("2.42") apply (false)
-    id("com.squareup.sqldelight").version ("1.5.4") apply (false)
+    id("com.android.application").version("7.4.2").apply(false)
+    id("com.android.library").version("7.4.2").apply(false)
+    kotlin("android").version("1.8.0").apply(false)
+    kotlin("multiplatform").version("1.8.0").apply(false)
+    id ("com.google.dagger.hilt.android").version ("2.44") apply (false)
+    id("com.squareup.sqldelight").version ("1.5.5") apply (false)
 }
 
 tasks.register("clean", Delete::class) {
