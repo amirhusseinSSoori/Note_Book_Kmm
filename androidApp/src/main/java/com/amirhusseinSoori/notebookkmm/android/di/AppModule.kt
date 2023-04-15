b6e5d3d40cf4298d0fa65b43d741f007fcd790a8
amirss <amirhusseinsoori75@gmail.com>
2023-04-15 16:49:05 +0330

    initial mainModule

--- androidApp/src/main/java/com/amirhusseinSoori/notebookkmm/android/di/AppModule.kt
+++ androidApp/src/main/java/com/amirhusseinSoori/notebookkmm/android/di/AppModule.kt
@@ -1,31 +1,23 @@
 package com.amirhusseinSoori.notebookkmm.android.di
 
-import android.app.Application
+
+import com.amirhusseinSoori.notebookkmm.android.note_detail.NoteDetailViewModel
+import com.amirhusseinSoori.notebookkmm.android.note_list.NoteListViewModel
 import com.amirhusseinSoori.notebookkmm.data.local.DatabaseDriverFactory
 import com.amirhusseinSoori.notebookkmm.data.note.SqlDelightNoteDataSource
 import com.amirhusseinSoori.notebookkmm.database.NoteDatabase
 import com.amirhusseinSoori.notebookkmm.domain.note.NoteDataSource
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.dsl.module
 
-import com.squareup.sqldelight.db.SqlDriver
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
-@Module
-@InstallIn(SingletonComponent::class)
-object AppModule {
-
-    @Provides
-    @Singleton
-    fun provideSqlDriver(app: Application): SqlDriver {
-        return DatabaseDriverFactory(app).createDriver()
+val mainModule = module {
+    factory<NoteDataSource> {
+        SqlDelightNoteDataSource(NoteDatabase(get()))
     }
+    single { DatabaseDriverFactory(androidContext()).createDriver() }
 
-    @Provides
-    @Singleton
-    fun provideNoteDataSource(driver: SqlDriver): NoteDataSource {
-        return SqlDelightNoteDataSource(NoteDatabase(driver))
-    }
+    viewModelOf(::NoteListViewModel)
+    viewModelOf(::NoteDetailViewModel)
 }
