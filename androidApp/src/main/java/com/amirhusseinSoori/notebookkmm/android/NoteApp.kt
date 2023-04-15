b8a708f56ff86e296341711078f4b168e4c4f4f5
amirss <amirhusseinsoori75@gmail.com>
2023-04-15 16:50:01 +0330

    initial NoteApp with koin

--- androidApp/src/main/java/com/amirhusseinSoori/notebookkmm/android/NoteApp.kt
+++ androidApp/src/main/java/com/amirhusseinSoori/notebookkmm/android/NoteApp.kt
@@ -1,7 +1,16 @@
 package com.amirhusseinSoori.notebookkmm.android
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
+import com.amirhusseinSoori.notebookkmm.android.di.mainModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
-@HiltAndroidApp
-class NoteApp: Application()
+class NoteApp: Application(){
+    override fun onCreate() {
+        super.onCreate()
+        startKoin{
+            androidContext(this@NoteApp)
+            modules(mainModule)
+        }
+    }
+}
